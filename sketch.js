335de0e6e65b3454541ce7a09b47d2ef0a6da646
scardinanoire <andre.corsi@outlook.it>
2022-10-14 17:09:49 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
-let tubi = [];
-let numtub = 25;
-let distbord = 20;
+let tubi = []; //array that contains the objects moving
+let numtub = 0; //initial number of the balls
+let distbord = 20; //distance from the border in wich the ball bounce
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   background(0, 0, 0);
+
+  numtub = random(2, 5);
 
   for (let i = 0; i < numtub; i++) {
     tubi.push(
@@ -20,7 +22,25 @@ function setup() {
 }
 
 function draw() {
+  if (round(random(0, 50)) == 25) {
+    tubi.push(
+      new tube(
+        random(0, width - distbord),
+        random(0, height - distbord),
+        round(random(10, 50)),
+        random(1, 4)
+      )
+    );
+  }
+  if (tubi.length > 15 && round(random(0, 50)) == 24) {
+    tubi.pop();
+  }
+
   background(0, 0, 0, 5);
+  if (frameCount % round(random(15, 25)) == 0) {
+    background(0, 0, 0, random(15, 25));
+  }
+
   for (let i = 0; i < tubi.length; i++) {
     tubi[i].move();
   }
@@ -28,6 +48,7 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
 }
 
 class tube {
@@ -58,15 +79,19 @@ class tube {
     switch (this.case) {
       case 1:
         this.x += this.v;
+        this.v = this.v + random(-0.05, 0.05);
         break;
       case 2:
         this.x -= this.v;
+        this.v = this.v + random(-0.05, 0.05);
         break;
       case 3:
         this.y += this.v;
+        this.v = this.v + random(-0.05, 0.05);
         break;
       case 4:
         this.y -= this.v;
+        this.v = this.v + random(-0.05, 0.05);
         break;
     }
 
